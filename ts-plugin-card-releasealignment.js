689e6319ffb502be2c645a89107ef5c647e78ff9
John Martin <curmudgeon@rallydev.com>
2013-10-08 19:29:08 -0700

    change text to # User Stories Not in Release

--- ts-plugin-card-releasealignment.js
+++ ts-plugin-card-releasealignment.js
@@ -121,7 +121,7 @@ Ext.define('Rally.ui.cardboard.plugin.ReleaseAlignment', {
                             var html = "";
                             var out_of_sync_total = 0;
                             if ( records.length > 0 ) {
-                                html = "# items misaligned: <span class='status-warn'>" + records.length + "</span>";
+                                html = "# User Stories Not in Release: <span class='status-warn'>" + records.length + "</span>";
                             } 
                             Ext.query('#' + feature_fid + '-releasealignment')[0].innerHTML = html;
                             
